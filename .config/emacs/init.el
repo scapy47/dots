c0cb3e51faf0acea6f5f70e2971fa40051898692
scapy47 <134828740+scapy47@users.noreply.github.com>
2026-03-18 11:46:33 +0530

    (evil mode 1)

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,3 +2,11 @@
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (column-number-mode 1)
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
